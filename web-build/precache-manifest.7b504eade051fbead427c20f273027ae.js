82a85c1f8f2c280ce3c8a4d0690fa34c2cb62f70
Alexander Talberg <alexandermtalberg@gmail.com>
2020-09-21 12:35:05 -0600

    webpack

---
+++ web-build/precache-manifest.7b504eade051fbead427c20f273027ae.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "849b5084651be2fa0ac3",
+    "url": "/static/js/app.b2fe2930.chunk.js"
+  },
+  {
+    "revision": "66090dc4495d4ce3dfb9",
+    "url": "/static/js/runtime~app.2e9f1821.js"
+  },
+  {
+    "revision": "1a1efba75d1e8e3ea6f6",
+    "url": "/static/js/2.59aecdf3.chunk.js"
+  },
+  {
+    "revision": "53ff10af5b1187dd8aad268ee4a9c965",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "e802d2644326fd9fa701f1d969a8a94a",
+    "url": "/register-service-worker.js"
+  },
+  {
+    "revision": "a990a8c98c379b1c14de33443329d80a",
+    "url": "/static/js/2.59aecdf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d324ddc823eca6c86688f62576c81e76",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "e72daee58e37c99d3553059eda418aa8",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "b27287954a8393ef9c70073b2fb0b988",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "426b150e2a2d2da2ab79a77a81ab3003",
+    "url": "/manifest.json"
+  }
+];
